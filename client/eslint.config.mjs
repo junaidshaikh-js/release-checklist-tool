b27cb70bc4f4abc98cc28a309ab2d1b025845bbd
Junaid Shaikh <sj.shaikhjunaid@gmail.com>
2026-03-22 11:25:24 +0530

    scaffold client

---
+++ client/eslint.config.mjs
@@ -0,0 +1,22 @@
+import { defineConfig, globalIgnores } from "eslint/config";
+import js from "@eslint/js";
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+
+const eslintConfig = defineConfig([
+  // Base ESLint recommended rules
+  js.configs.recommended,
+  // Next.js recommended rules (core web vitals + TypeScript)
+  ...nextVitals,
+  ...nextTs,
+  // Override default ignores of eslint-config-next.
+  globalIgnores([
+    // Default ignores of eslint-config-next:
+    ".next/**",
+    "out/**",
+    "build/**",
+    "next-env.d.ts",
+  ]),
+]);
+
+export default eslintConfig;
